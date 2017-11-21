23cf6ce0099f08f7d8b1ad4dec5e22e4b5691c60
Alex van Poppelen <avanpoppelen@gmail.com>
2017-11-22 00:36:06 +0100

    migrate polybar and rofi to .env

--- setup.sh
+++ setup.sh
@@ -4,7 +4,11 @@
 cd ~
 mkdir downloads
 mkdir -p .config/i3
+mkdir -p .config/polybar
+mkdir -p .config/rofi
 
 # setup symlinks
 ln -s .env/.Xresources .Xresources
 ln -s .env/i3/config .config/i3/config
+ln -s .env/polybar/config .config/polybar/config
+ln -s .env/rofi/config .config/rofi/config
