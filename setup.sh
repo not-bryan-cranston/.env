6a609ce046658cb0f4abeaff27f5fe403b07258e
Alex van Poppelen <avanpoppelen@gmail.com>
2018-01-02 23:21:23 +0100

    move dotfiles

--- setup.sh
+++ setup.sh
@@ -9,10 +9,10 @@ mkdir -p .config/rofi
 mkdir -p .config/zathura
 
 # setup symlinks
-ln -s .env/.bash_profile .bash_profile
+ln -s .env/bash_profile .bash_profile
 ln -s .env/bashrc .bashrc
 
-ln -s .env/.Xresources .Xresources
+ln -s .env/Xresources .Xresources
 ln -s .env/i3/config .config/i3/config
 ln -s .env/polybar/config .config/polybar/config
 ln -s .env/rofi/config .config/rofi/config
