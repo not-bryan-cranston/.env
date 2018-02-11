76ba8d4b476867fd83790cd057b8bc2e599807d0
Alex van Poppelen <avanpoppelen@gmail.com>
2018-02-11 19:30:38 +0000

    automate wireless interface for polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,10 +6,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+# Get wireless interface.
+wi="$(iw dev | grep Interface | sed -n 1p | cut -d ' ' -f 2)"
+
 # Launch bar(s)
 if type "xrandr"; then
 	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-		MONITOR=$m polybar main &
+		MONITOR=$m WIRELESS_INTERFACE=$wi polybar main &
 	done
 else
 	polybar main &
