712b59b1fb5e02b9355a6dbdacfe9c0136800f77
Alex van Poppelen <avanpoppelen@gmail.com>
2018-04-07 12:52:19 +0100

    a bunch of fixes

--- setup.sh
+++ setup.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-# install gui software
-sudo pacman -S i3
+distro=$(cat /etc/os-release |& grep '^ID=' | cut -d '=' -f 2)
 
-# install tools
-sudo pacman -S nmap gdb
+# install software
+# TODO: actually test this and add missing software
+if [ "$distro" = "arch" ]; then
+	# desktop environment
+	sudo pacman -S --noconfirm i3 rofi ranger zathura compton xautolock scrot ttf-dejavu ttf-font-awesome 
+	# tools
+	sudo pacman -S --noconfirm gvim git gdb ltrace strace rsync nmap traceroute
+	# software
+	sudo pacman -S --noconfirm firefox keepassx2 irssi transmission openvpn
+	# media
+	sudo pacman -S --noconfirm mpd mpc ncmpcpp mpv gimp 
+fi
 
 # create folders
 mkdir -p downloads
@@ -14,17 +23,17 @@ mkdir -p .config/rofi
 mkdir -p .config/zathura
 
 # link dotfiles
-ln -s .env/bash_profile .bash_profile
-ln -s .env/bashrc .bashrc
+ln -s ~/.env/bash_profile .bash_profile
+ln -s ~/.env/bashrc .bashrc
 
-ln -s .env/vimrc .vimrc
+ln -s ~/.env/vimrc .vimrc
 
-ln -s .env/gdbinit .gdbinit
+ln -s ~/.env/gdbinit .gdbinit
 
-ln -s .env/Xresources .Xresources
+ln -s ~/.env/Xresources .Xresources
 
 # link various configs
-ln -s .env/i3/config .config/i3/config
-ln -s .env/polybar/config .config/polybar/config
-ln -s .env/rofi/config .config/rofi/config
-ln -s .env/zathura/zathurarc .config/zathura/zathurarc
+ln -s ~/.env/i3/config .config/i3/config
+ln -s ~/.env/polybar/config .config/polybar/config
+ln -s ~/.env/rofi/config .config/rofi/config
+ln -s ~/.env/zathura/zathurarc .config/zathura/zathurarc
